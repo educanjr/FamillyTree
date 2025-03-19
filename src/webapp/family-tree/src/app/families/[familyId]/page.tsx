0facd4873b6265b8f3c0105461f5b1b032cb4aee
educanjr <eduardo.canaljunior@gmail.com>
2025-03-19 01:20:59 -0300

    Fix error using route params

--- src/webapp/family-tree/src/app/families/[familyId]/page.tsx
+++ src/webapp/family-tree/src/app/families/[familyId]/page.tsx
@@ -1,6 +1,5 @@
 import { fetchFamilyWithMembers } from "@/api-queries/fetchFamilyWithMembers";
 import FamilyGrid from "@/componets/family-grid/family-grid";
-import { FamilyApiResponse } from "@/Interfaces/familyApiResponse";
 
 import styles from './page.module.css';
 import GoHome from "@/componets/go-home/go-home";
@@ -11,18 +10,35 @@ interface PageProps {
 }
 
 export default async function FamilyPage({params}: PageProps) {
-    let family: FamilyApiResponse | null = null;
-    try {        
-        family = await fetchFamilyWithMembers(params.familyId);
-    } catch (error) {
-        console.error("Error fetching family:", error);
-    }
-
+    const { familyId } = await params;
     return (        
         <>
             <div className={styles['button-container']}>
                 <GoHome />
             </div>
+            <FamilyData familyId={familyId} />
+        </>
+    )
+}
+
+interface FamilyDataProps {
+    familyId: string;
+}
+
+async function tryFetchFamilyData(familyId: string) {
+    try {
+        return await fetchFamilyWithMembers(familyId);
+    } catch (error) {
+        console.error("Error fetching family:", error);
+        return null;
+    }
+}
+
+async function FamilyData({familyId}: FamilyDataProps) {
+    const family = await tryFetchFamilyData(familyId);
+
+    return (
+        <>
             {family && family?.members && (<FamilyGrid familyMembers={family?.members} />)}
         </>
     )
